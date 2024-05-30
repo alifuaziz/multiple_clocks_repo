fca504e7fbe1ecba5acb9d3d60034d3a59c314ef
Alif Aziz <alif.aziz@icloud.com>
2024-05-30 17:37:22 +0100

    Merged Branch...

--- mc/replay_analysis/scripts/script_data_rdm.py
+++ mc/replay_analysis/scripts/script_data_rdm.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
+import seaborn as sns 
 from joblib import Parallel, delayed
 from pathlib import Path
 from tqdm import tqdm
@@ -20,18 +21,32 @@ import mc.replay_analysis.functions.data_rdms as data_rdms
 import mc.analyse.analyse_MRI_behav as analyse_MRI_behav
 from mc.analyse.searchlight import get_volume_searchlight
 
-#%% Define the Subject data and the EVs that are being used to create the RDMs
-# SUB = "sub-02"
-# EVS_TYPE = 'instruction_period'
-# MODEL_TYPE = 'replay'
-# RDM_SIZE = "cross_corr"
-# TASK_HALVES = ['1', '2']
-# RDM_VERSION = '01'
+# %% Define the Subject data and the EVs that are being used to create the RDMs
+SUB = "sub-03"
+EVS_TYPE = 'instruction_period'
+MODEL_TYPE = 'replay'
+RDM_SIZE = "cross_corr"
+TASK_HALVES = ['1', '2']
+RDM_VERSION = '01'
+
+# LOADING IN PICK FILES 
+LOAD_VOLU_SEARCHLIGHT = False
+LOAD_DATA_SEARCHLIGHT = False
+LOAD_DATA_RDM = False
 
 
-# SUBJECT_DIRECTORY = '/Users/student/PycharmProjects/data/derivatives/' + SUB + '/'
-# RESULTS_DIRECTORY = '/Users/student/PycharmProjects/data/derivatives/' + SUB + '/func/RSA_replay/'
 
+SUBJECT_DIRECTORY = Path('/home/fs0/chx061/scratch/data/derivatives/' + SUB + '/')
+RESULTS_DIRECTORY = Path('/home/fs0/chx061/scratch/data/derivatives/' + SUB + '/func/RSA_replay/')
+# Create the directories if they do not exist
+if not RESULTS_DIRECTORY.exists():
+    RESULTS_DIRECTORY.mkdir(parents=True)
+if not SUBJECT_DIRECTORY.exists():
+    SUBJECT_DIRECTORY.mkdir(parents=True)
+
+
+
+# %%
 
 
 def replay_analysis(**analysis_options):
@@ -41,15 +56,24 @@ def replay_analysis(**analysis_options):
     EVS_TYPE    = analysis_options['analysis_options'].get('EVS_TYPE', 'instruction_period')
     MODEL_TYPE  = analysis_options['analysis_options'].get('MODEL_TYPE', 'replay')
     RDM_SIZE    = analysis_options['analysis_options'].get('RDM_SIZE', 'cross_corr')
+# %%
+    SUB         = analysis_options['analysis_options'].get('SUB', 'sub-02')
+    EVS_TYPE    = analysis_options['analysis_options'].get('EVS_TYPE', 'instruction_period')
+    MODEL_TYPE  = analysis_options['analysis_options'].get('MODEL_TYPE', 'replay')
+    RDM_SIZE    = analysis_options['analysis_options'].get('RDM_SIZE', 'cross_corr')
     # TASK_HALVES = analysis_options.get('TASK_HALVES', ['1', '2'])
+    RDM_VERSION = analysis_options['analysis_options'].get('RDM_VERSION', '01')
     RDM_VERSION = analysis_options['analysis_options'].get('RDM_VERSION', '01')
 
 
     # Directory for the data and results
     SUBJECT_DIRECTORY = analysis_options['analysis_options'].get('SUBJECT_DIRECTORY', Path('/Users/student/PycharmProjects/data/derivatives/' + SUB + '/'))
     RESULTS_DIRECTORY = analysis_options['analysis_options'].get('RESULTS_DIRECTORY', Path('/Users/student/PycharmProjects/data/derivatives/' + SUB + '/func/RSA_replay/'))
+    SUBJECT_DIRECTORY = analysis_options['analysis_options'].get('SUBJECT_DIRECTORY', Path('/Users/student/PycharmProjects/data/derivatives/' + SUB + '/'))
+    RESULTS_DIRECTORY = analysis_options['analysis_options'].get('RESULTS_DIRECTORY', Path('/Users/student/PycharmProjects/data/derivatives/' + SUB + '/func/RSA_replay/'))
 
 
+    print(SUBJECT_DIRECTORY)
     print(SUBJECT_DIRECTORY)
     # Create the directories if they do not exist
     if not RESULTS_DIRECTORY.exists():
@@ -115,6 +139,7 @@ def replay_analysis(**analysis_options):
         threshold = 0.5)
 
     # Deals with searchlights that are not the correct size
+    # Deals with searchlights that are not the correct size
     vol_neighbors = data_rdms.resize_neighbors(
         vol_neighbors = vol_neighbors,
         size = 93
@@ -137,6 +162,15 @@ def replay_analysis(**analysis_options):
     #     vol_searchlight = pickle.load(f)
 
 
+    # %% Save the vol_searchlight to a pickle file
+    with open(f"{SUBJECT_DIRECTORY}/vol_searchlight_df.pkl", 'wb') as f:
+        pickle.dump(vol_searchlight, f)
+
+    # load vol_searchlight from a pickle file
+    # with open(f"{SUBJECT_DIRECTORY}/vol_searchlight_df.pkl", 'rb') as f:
+    #     vol_searchlight = pickle.load(f)
+
+
     #%% Create the data searchlights from the vol_searchlights
 
     data_searchlight = data_rdms.get_data_searchlight(
@@ -148,8 +182,12 @@ def replay_analysis(**analysis_options):
     # save data_searchlight to a pickle file
     # with open(f"{SUBJECT_DIRECTORY}/searchlight_data_searchlight.pkl", 'wb') as f:
     #     pickle.dump(data_searchlight, f)
+    # with open(f"{SUBJECT_DIRECTORY}/searchlight_data_searchlight.pkl", 'wb') as f:
+    #     pickle.dump(data_searchlight, f)
 
     # load data_searchlight from a pickle file
+    with open(f"{SUBJECT_DIRECTORY}/searchlight_data_searchlight.pkl", 'rb') as f:
+        data_searchlight = pickle.load(f)
     with open(f"{SUBJECT_DIRECTORY}/searchlight_data_searchlight.pkl", 'rb') as f:
         data_searchlight = pickle.load(f)
 
@@ -159,6 +197,17 @@ def replay_analysis(**analysis_options):
         data_searchlight = data_searchlight,
         SIZE = RDM_SIZE
         )
+
+    #%% Save the data RDMs to a pickle file
+
+    # save data_rdms_dict to a pickle file
+    with open(f"{SUBJECT_DIRECTORY}/searchlight_data_rdms.pkl", 'wb') as f:
+        pickle.dump(data_rdms_dict, f)
+
+    # load data_rdms_dict from a pickle file
+    # with open(f"{SUBJECT_DIRECTORY}/searchlight_data_rdms.pkl", 'rb') as f:
+    #     data_rdms_dict = pickle.load(f)
+
 
     #%% Save the data RDMs to a pickle file
 
